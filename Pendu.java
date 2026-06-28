bf0ca0ca94ef11960a4ec55a2f4401a24b44357e
starikouedraogo <starikouedraogo@gmail.com>
2026-06-28 19:48:43 +0000

    Correction du fichier Pendu.java

--- Pendu.java
+++ Pendu.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class Pendu {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     String choix = "";
     do {    jouerPartie();  
             System.out.println("Voulez-vous rejouer ? (O/N)");
